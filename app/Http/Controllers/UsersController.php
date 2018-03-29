c2b3907a1f3747479d9e0d139e3c40487d1cc4cd
luke <luke_44@foxmail.com>
2018-03-29 10:23:53 +0800

    User profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller {
+	public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+	public function update() {}
+	public function edit() {}
+}
