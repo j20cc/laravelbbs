d30651bca4138c8212f1c3a77be954010572d725
luke <luke_44@foxmail.com>
2018-03-29 16:05:47 +0800

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -43,11 +43,18 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span> 个人中心
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> 编辑资料
+                                </a>
                             </li>
                             <li>
                                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    退出登录
+                                    <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> 退出登录
                                 </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     {{ csrf_field() }}
