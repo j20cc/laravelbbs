ebd5c9064a4f5a39780b7f8a8c8b393a9f856b5a
luke <luke_44@foxmail.com>
2018-03-29 16:33:39 +0800

    使用编辑器

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@endsection
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.min.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.min.js') }}"></script>
+
+    <script>
+    $(document).ready(function(){
+        var editor = new Simditor({
+            textarea: $('#editor'),
+        });
+    });
+    </script>
+
+@endsection
+
 @section('content')
 
 <div class="container">
@@ -26,30 +46,29 @@
                 @else
                     <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
                 @endif
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <div class="form-group">
+                            <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
+                        </div>
 
-                    <div class="form-group">
-                        <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
-                    </div>
+                        <div class="form-group">
+                            <select class="form-control" name="category_id" required>
+                                <option value="" hidden disabled selected>请选择分类</option>
+                                @foreach ($categories as $value)
+                                    <option value="{{ $value->id }}">{{ $value->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                    <div class="form-group">
-                        <select class="form-control" name="category_id" required>
-                            <option value="" hidden disabled selected>请选择分类</option>
-                            @foreach ($categories as $value)
-                                <option value="{{ $value->id }}">{{ $value->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                        <div class="form-group">
+                            <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                        </div>
 
-                    <div class="form-group">
-                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
-                    </div>
-
-                    <div class="well well-sm">
-                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
-                    </div>
-                </form>
+                        <div class="well well-sm">
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
+                        </div>
+                    </form>
             </div>
         </div>
     </div>
