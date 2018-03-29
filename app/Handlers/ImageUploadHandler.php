646b4f743cd86a6365fa6a74e4f8c503d95572fc
luke <luke_44@foxmail.com>
2018-03-29 11:52:13 +0800

    User upload avatar

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Handlers;
+
+use Image;
+
+class ImageUploadHandler
+{
+    protected $allowed_ext = ["png", "jpg", "gif", "jpeg"];
+    protected $allowed_size = 1024*1024*2;  //2M
+
+    public function save($file, $folder, $file_prefix, $max_width = false)
+    {
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
+        $upload_path = public_path() . '/' . $folder_name;
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+        $size = intval($file->getClientSize());
+        $file_name = $file_prefix . '_' . time() . '_' . str_random() . '.' . $extension;
+
+        if( ! in_array($extension, $this->allowed_ext) ) {
+            return false;
+        }
+        if( $size > $this->allowed_size ) {
+            return false;
+        }
+
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
+        $file->move($upload_path, $file_name);
+
+        return [
+            'path' => config('app.url'). "/$folder_name/$file_name"
+        ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
+    }
+}
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -27,7 +27,8 @@ class UserRequest extends FormRequest
         return [
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
-            'introduction' => 'max:80'
+            'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200'
         ];
     }
 
@@ -37,7 +38,9 @@ class UserRequest extends FormRequest
             'name.required' => '用户名不能为空',
             'name.between' => '用户名长度必须在3-25之间',
             'name.regex' => '用户名应当是"数字","英文","-","_"的组合',
-            'name.unique' => '用户名被占用'
+            'name.unique' => '用户名被占用',
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上'
         ];
     }
 }
