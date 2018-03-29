8ec0ba7661b2aca6557772f23fe016b5c8c66fa9
luke <luke_44@foxmail.com>
2018-03-29 17:14:45 +0800

    防止xss

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,16 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
-    }
-
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
     }
 }
