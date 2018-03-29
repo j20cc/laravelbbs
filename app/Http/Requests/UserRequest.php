66378c5d455d963d1dc6e6ef46523861a9a1b653
luke <luke_44@foxmail.com>
2018-03-29 11:07:34 +0800

    User profile editing

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '用户名不能为空',
+            'name.between' => '用户名长度必须在3-25之间',
+            'name.regex' => '用户名应当是"数字","英文","-","_"的组合',
+            'name.unique' => '用户名被占用'
+        ];
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'LaraBBS') - Laravel/PHP 社区</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -21,6 +21,7 @@
 
         <div class="container">
 
+            @include('layouts._message')
             @yield('content')
 
         </div>
