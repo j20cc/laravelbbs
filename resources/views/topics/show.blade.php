140a96a7526486b4038fe9f72cb46ad4e3a65951
lukedever <luke_44@foxmail.com>
2018-04-02 02:07:15 +0000

    complete front

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -67,8 +67,7 @@
         {{-- 用户回复列表 --}}
         <div class="panel panel-default topic-reply">
             <div class="panel-body">
-                {{-- 登陆才可以回复 --}}
-                @includeWhen(Auth::check(), 'topics._reply_box', ['topic'=>$topic])
+                @include('topics._reply_box', ['topic'=>$topic])
                 @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->recent()->get()])
             </div>
         </div>
