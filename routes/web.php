dd2b7a9b410c816871cf045c742636b20f0900b6
lukedever <luke_44@foxmail.com>
2018-04-06 02:51:27 +0000

    设置默认首页

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 Route::get('test', 'PagesController@test');
 
 // Authentication Routes...
