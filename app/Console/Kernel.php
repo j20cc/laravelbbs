5006510949ea04b54cb2d623c6f354751a560b17
lukedever <luke_44@foxmail.com>
2018-04-06 02:42:25 +0000

    记录用户最后活跃时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,8 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         // 一小时执行一次『活跃用户』数据生成的命令
         $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每日零时执行一次
+        $schedule->command('larabbs:sync-user-actived-at')->dailyAt('00:00');
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
         notify as protected laravelNotify;
     }
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
     public function notify($instance)
     {
